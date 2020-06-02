d411c69f9a2b2893f49de288df0fcb250b0f3506
zhangirisz <zhangirisz@gmail.com>
2020-06-01 19:16:55 -0500

    Displayed course data on course details page

---
+++ utflow/signup_routes.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, url_for, flash, redirect, request, jsonify, json
+from flask_jwt_extended import (create_access_token)
+from utflow.models import *
+from utflow import app, db, bcrypt, jwt
+
+@app.route('/api/signup', methods=['POST'])
+def register():
+    first_name = request.get_json()['first_name']
+    last_name = request.get_json()['last_name']
+    email = request.get_json()['email']
+    major = request.get_json()['major']
+    password = bcrypt.generate_password_hash(
+        request.get_json()['password']).decode('utf-8')
+    dept = Dept.query.filter_by(name=major).first()
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        result = jsonify({"error": "An account already exists for this email"})
+    else:
+        user = User(first_name=first_name, last_name=last_name, 
+        email=email, password=password, major_id=dept.id)
+        db.session.add(user)
+        db.session.commit()
+
+        result_user = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'password': password,
+            'major': major
+        }
+        result = jsonify({'result': result_user})
+
+    return result
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    email = request.get_json()['email']
+    password = request.get_json()['password']
+
+    user = User.query.filter_by(email=email).first()
+    major = Dept.query.filter_by(id=user.major_id).first()
+    if user and bcrypt.check_password_hash(user.password, password):
+        access_token = create_access_token(identity={
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+            'major': major.name
+        })
+        result = access_token
+    else:
+        result = jsonify({"error": "Invalid username and password"})
+
+    return result
