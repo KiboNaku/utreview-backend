898610e735c7167293240ca65cda21422b5e47a2
Iris Zhang <zhangirisz@gmail.com>
2020-06-03 13:45:31 -0500

    Update routes.py

--- utflow/routes.py
+++ utflow/routes.py
@@ -60,13 +60,51 @@ def getProfs():
     result = jsonify({"professors": results})
     return result
 
+@app.route('/api/review_feedback', methods=['POST'])
+def review_feedback():
+    is_like = request.get_json()['like']
+    is_remove = request.get_json()['remove']
+    user_email = request.get_json()['userEmail']
+    review_id = request.get_json()['reviewId']
+
+    user = User.query.filter_by(email=user_email).first()
+
+    if(is_remove):
+        if(is_like):
+            review_like = ReviewLiked.query.filter_by(user_id=user.id, review_id=review_id)
+            db.session.delete(review_like)
+            db.session.commit()
+        else:
+            review_dislike = ReviewDisliked.query.filter_by(user_id=user.id, review_id=review_id)
+            db.session.delete(review_dislike)
+            db.session.commit()
+    else:
+        if(is_like):
+            review_like = ReviewLiked(user_id=user.id, review_id=review_id)
+            db.session.add(review_like)
+            db.session.commit()
+        else:
+            review_dislike = ReviewDisliked(user_id=user.id, review_id=review_id)
+            db.session.add(review_dislike)
+            db.session.commit()
+
+    result = jsonify({"result": 'success'})
+    return result
 
 @app.route('/api/course_info', methods=['POST'])
 def course_info():
     course_name = request.get_json()['courseNum']
+    logged_in = request.get_json()['loggedIn']
+    user_email = request.get_json()['userEmail']
+    if(logged_in):
+        curr_user = User.query.filter_by(email=user_email).first()
     course_parsed = course_name.split()
-    course_abr = course_parsed[0]
-    course_no = course_parsed[1]
+    if(len(course_parsed) == 3):
+        course_abr = course_parsed[0] + " " + course_parsed[1]
+        course_no = course_parsed[2]
+    else:
+        course_abr = course_parsed[0]
+        course_no = course_parsed[1]
 
     course_dept = Dept.query.filter_by(abr=course_abr).first()
     course = Course.query.filter_by(
@@ -85,6 +123,7 @@ def course_info():
         eCIS = scores[0].avg
 
     reviews = course.reviews
+    review_list = []
     if(len(reviews) == 0):
         percentLiked = None
         difficulty = None
@@ -103,6 +142,41 @@ def course_info():
             difficulty += rating.difficulty
             usefulness += rating.usefulness
             workload += rating.workload
+            user = User.query.filter_by(id=reviews[i].user_posted).first()
+            prof = Prof.query.filter_by(id=reviews[i].professor_id).first()
+            user_dept = Dept.query.filter_by(id=user.dept_id).first()
+            num_liked = 0
+            num_disliked = 0
+            like_pressed = False
+            dislike_pressed = False
+            for like in reviews[i].users_liked:
+                num_liked += 1
+                if(logged_in):
+                    if(curr_user.id == like.user_id):
+                        like_pressed = True
+
+            for dislike in reviews[i].users_disliked:
+                num_disliked += 1
+                if(logged_in):
+                    if(curr_user.id == dislike.user_id):
+                        dislike_pressed = True
+            review_object = {
+                'key': reviews[i].id,
+                'review': reviews[i].course_review,
+                'liked': rating.approval,
+                'usefulness': rating.usefulness,
+                'difficulty': rating.difficulty,
+                'workload': rating.workload,
+                'userMajor': user_dept.name,
+                'profPic': "https://images.dog.ceo/breeds/pembroke/n02113023_12785.jpg",
+                'profName': prof.name,
+                'numLiked': num_liked,
+                'numDisliked': num_disliked,
+                'likePressed': like_pressed,
+                'dislikePressed': dislike_pressed,
+                'date': reviews[i].date_posted.strftime("%m/%d/%Y")
+            }
+            review_list.append(review_object)
         percentLiked = round(percentLiked/len(reviews), 2) * 100
         difficulty = round(difficulty/len(reviews), 1)
         usefulness = round(usefulness/len(reviews), 1)
@@ -143,6 +217,7 @@ def course_info():
 
     result = jsonify({"course_info": course_info,
                       "course_rating": course_rating,
-                      "course_profs": prof_list})
+                      "course_profs": prof_list, 
+                      "course_reviews": review_list})
 
     return result
