da94efe846db0afefa670d966cd61917c0066ce6
Andy Ni <nakukibo@gmail.com>
2020-07-01 17:52:43 -0500

    fetch_courses: changed to list

--- utreview/services/fetch_course_info.py
+++ utreview/services/fetch_course_info.py
@@ -9,7 +9,7 @@ KEY_TOPIC_NUM = "topic_num"
 KEY_PRE_REQ = "pre_req"
 
 
-def fetch_courses(file_name, sheet_name):
+def fetch_courses(file_name, sheet_lst):
 	"""Parse the course info excel sheet 
 
 	Args:
@@ -18,33 +18,35 @@ def fetch_courses(file_name, sheet_name):
 	Returns:
 		[type]: [description]
 	"""
-	df = pd.read_excel(
-		file_name, sheet_name=sheet_name)
-	df.reset_index()
-
 	courses = []
-	count = 0
 
-	for index, row in df.iterrows():
+	for sheet_name in sheet_lst:
+			
+		df = pd.read_excel(
+			file_name, sheet_name=sheet_name)
+		df.reset_index()
 
-		__field_of_study = "Field of Study"
-		__course_num = "Course Number"
-		__catalog_title = "Catalog Title"
-		__description = "Subject Matter Description"
-		__restriction = "Restrictive Statement"
-		__topic_num = "Topic Number"
-		__pre_req = "Prerequisites"
 
-		course = {
-			KEY_DEPT: row[__field_of_study].strip(),
-			KEY_NUM: row[__course_num].strip(),
-			KEY_TITLE: row[__catalog_title].strip()[:-1],
-			KEY_DESCRIPTION: row[__description].strip(),
-			KEY_RESTRICTION: row[__restriction].strip(),
-			KEY_TOPIC_NUM: parse_topic(row[__topic_num]),
-			KEY_PRE_REQ: row[__pre_req].strip()
-		}
-		courses.append(course)
+		for index, row in df.iterrows():
+
+			__field_of_study = "Field of Study"
+			__course_num = "Course Number"
+			__catalog_title = "Catalog Title"
+			__description = "Subject Matter Description"
+			__restriction = "Restrictive Statement"
+			__topic_num = "Topic Number"
+			__pre_req = "Prerequisites"
+
+			course = {
+				KEY_DEPT: row[__field_of_study].strip(),
+				KEY_NUM: row[__course_num].strip(),
+				KEY_TITLE: row[__catalog_title].strip()[:-1],
+				KEY_DESCRIPTION: row[__description].strip(),
+				KEY_RESTRICTION: row[__restriction].strip(),
+				KEY_TOPIC_NUM: parse_topic(row[__topic_num]),
+				KEY_PRE_REQ: row[__pre_req].strip()
+			}
+			courses.append(course)
 	return courses
 
 
