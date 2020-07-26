81a1d17a21fe0bec989f7cb7108a996f4a52face
Andy Ni <nakukibo@gmail.com>
2020-07-26 15:42:03 -0500

    implemented secondary thread

--- app.py
+++ app.py
@@ -4,44 +4,34 @@ from utreview.models import *
 from utreview.routes import *
 from utreview.services import *
 from utreview.database.populate_database import *
+import logging
+import threading
+import time
 
 
-def __get_topic_zero(topic_courses):
+def thread_function(name):
+    
+    from utreview.services.fetch_ftp import fetch_ftp_files, fetch_sem_values, parse_ftp
+    from utreview.database.populate_database import populate_scheduled_course
+    
+    fetch_ftp_files('input_data') 
+    fetch_sem_values("input_data", "")
 
-	for topic_course in topic_courses:
-		if topic_course.topic_num == 0:
-			return topic_course
-	return None
+    depts = fetch_depts()
+    populate_dept(depts, override=True) 
+    dept_info = fetch_dept_info('input_data/Data_Requests.xlsx', [0, 1, 2])
+    populate_dept_info(dept_info)
 
-if __name__ == '__main__':
+    courses = fetch_courses('input_data/Data_Requests.xlsx', [0, 1, 2])
+    populate_course(courses, cur_sem = int(sem_current))
 
-    # from utreview.services.fetch_ftp import fetch_ftp_files, parse_ftp
-    # fetch_ftp_files('input_data')
+    ftp_info = parse_ftp("input_data")
+    populate_scheduled_course(ftp_info)
 
-    # fetch semester values 
-    # from utreview.services.fetch_ftp import fetch_sem_values
-    # fetch_sem_values("input_data", "")
 
-    # fetch dept info----------------------
-    # depts = fetch_depts()
-    # populate_dept(depts, override=True)
+if __name__ == '__main__':    
 
-    # dept_info = fetch_dept_info('input_data/Data Requests.xlsx', [0, 1, 2])
-    # populate_dept_info(dept_info)
-
-    # ----------finish fetch dept info---------------
-
-    # fetch course info---------------------
-
-    # courses = fetch_courses('input_data/Data Requests.xlsx', [0, 1, 2])
-    # populate_course(courses, cur_sem = int(sem_current))
-
-    # --------------finish fetch course info---------------------
-
-    # fetch schedule info ----------------------
-    # from utreview.services.fetch_ftp import parse_ftp, fetch_ftp_files
-    # ftp_info = parse_ftp("input_data")
-    # from utreview.database.populate_database import populate_scheduled_course
-    # populate_scheduled_course(ftp_info)
-
+    x = threading.Thread(target=thread_function, args=(1,))
+    x.start()
     app.run(debug=True)
+    x.join()
