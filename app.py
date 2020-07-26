25d01e0daaa972c62d4f3b48b5549881357ed38b
Andy Ni <nakukibo@gmail.com>
2020-07-26 16:07:15 -0500

    commented out extra threading

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ def thread_function(name):
     fetch_ftp_files('input_data') 
     fetch_sem_values("input_data", "")
 
-    depts = fetch_depts()
-    populate_dept(depts, override=True) 
-    dept_info = fetch_dept_info('input_data/Data_Requests.xlsx', [0, 1, 2])
-    populate_dept_info(dept_info)
+    # depts = fetch_depts()
+    # populate_dept(depts, override=True) 
+    # dept_info = fetch_dept_info('input_data/Data_Requests.xlsx', [0, 1, 2])
+    # populate_dept_info(dept_info)
 
-    courses = fetch_courses('input_data/Data_Requests.xlsx', [0, 1, 2])
-    populate_course(courses, cur_sem = int(sem_current))
+    # courses = fetch_courses('input_data/Data_Requests.xlsx', [0, 1, 2])
+    # populate_course(courses, cur_sem = int(sem_current))
 
     ftp_info = parse_ftp("input_data")
     populate_scheduled_course(ftp_info)
@@ -31,7 +31,7 @@ def thread_function(name):
 
 if __name__ == '__main__':    
 
-    x = threading.Thread(target=thread_function, args=(1,))
-    x.start()
+    # x = threading.Thread(target=thread_function, args=(1,))
+    # x.start()
     app.run(debug=True)
-    x.join()
+    # x.join()
