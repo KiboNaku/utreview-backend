da6287135f6a1a1f42f0f7c5a56f496d17a262a7
Andy Ni <nakukibo@gmail.com>
2020-07-27 18:48:15 -0500

    Updated email html

--- utreview/routes/admission.py
+++ utreview/routes/admission.py
@@ -37,6 +37,7 @@ def register():
     """
 
     r_val = {'email': None, 'success': 0, 'error': None}
+    print('request', request.get_json())
 
     first_name = request.get_json()['first_name']
     last_name = request.get_json()['last_name']
@@ -45,6 +46,7 @@ def register():
     other_major = request.get_json()['other_major']
     password_hash = bcrypt.generate_password_hash(request.get_json()['password']).decode('utf-8')
 
+    print(f"'{major}'")
     major_id = None
     if(major != None and major != ""):
         dept = Dept.query.filter_by(name=major).first()
