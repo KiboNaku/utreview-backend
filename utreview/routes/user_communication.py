00b88a723b56124dd9d01819cb094df471e84588
Iris Zhang <zhangirisz@gmail.com>
2020-07-31 19:48:06 -0500

    Implement reporting comments feature

--- utreview/routes/user_communication.py
+++ utreview/routes/user_communication.py
@@ -29,3 +29,40 @@ def contact_us_message():
     mail.send(msg)
 
     return 'Feedback Received'
+
+@app.route('/api/report_comment', methods=['POST'])
+def report_comment():
+
+    review_id = request.get_json()['review_id']
+    is_course = request.get_json()['is_course']
+    selected_options = request.get_json()['selected_options']
+    other_selected = request.get_json()['other_selected']
+    other_option = request.get_json()['other_option']
+
+    if(is_course):
+        course_review = CourseReview.query.filter_by(id=review_id).first()
+        review_topic = "Course"
+        author_email = course_review.review.author.email
+        review_topic_name = course_review.course.dept.abr + " " + course_review.course.num
+        review_comments = course_review.comments
+    else:
+        prof_review = ProfReview.query.filter_by(id=review_id).first()
+        review_topic = "Professor"
+        author_email = prof_review.review.author.email
+        review_topic_name = prof_review.prof.first_name + " " + prof_review.prof.last_name
+        review_comments = prof_review.comments
+
+    if(not other_selected):
+        other_option = "N/A"
+
+    msg = Message(
+        'Report Comment',
+        sender=("UT Review", "utexas.review@gmail.com"),
+        recipients=["utexas.review@gmail.com"])
+
+    msg.html = render_template(
+        'report_comment.html', options_selected=selected_options, other_selected=other_selected, other_option=other_option, review_topic=review_topic, 
+        author_email=author_email, review_topic_name=review_topic_name, review_comments=review_comments, review_id=review_id)
+    mail.send(msg)
+
+    return 'Feedback Received'
