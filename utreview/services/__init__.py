1b236a0b2fe47dba41b93890a5938351ace55e75
Andy Ni <nakukibo@gmail.com>
2020-08-11 18:35:33 -0500

    Revert "Andy"

--- utreview/services/__init__.py
+++ utreview/services/__init__.py
@@ -1,7 +1,3 @@
-
-from .automate_backend import *
+from .fetch_web import *
 from .fetch_course_info import *
 from .fetch_ecis import *
-from .fetch_ftp import *
-from .fetch_prof import *
-from .fetch_web import *
