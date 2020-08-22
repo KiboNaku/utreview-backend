0a7f72159be865b275bd9309fad3263c3f6e17dc
Iris Zhang <zhangirisz@gmail.com>
2020-08-21 19:33:05 -0500

    Rearrange backend profile routes

--- utreview/routes/__init__.py
+++ utreview/routes/__init__.py
@@ -4,6 +4,7 @@ from .backend import *
 from .catalyst import *
 from .course_info import *
 from .get_info import *
+from .profile_info import *
 from .populate import *
 from .prof_info import *
 from .review_info import *
