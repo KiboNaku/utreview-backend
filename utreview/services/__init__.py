7338784864a6def5da00f86fc42df9cf27be8e3d
Andy Ni <nakukibo@gmail.com>
2020-08-30 20:55:38 -0500

    Updated services documentation

--- utreview/services/__init__.py
+++ utreview/services/__init__.py
@@ -4,3 +4,8 @@ from .fetch_ecis import *
 from .fetch_ftp import *
 from .fetch_prof import *
 from .fetch_web import *
+
+"""
+This package is for miscellaneous files for fetching data, automating backend, or logging information.
+Feel free to separate into more specific packages in the future.
+"""
